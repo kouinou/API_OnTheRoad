d19b7461e999ef78cca754a9f0659259b8d8a287
kouinou <aquinn.scotland@gmail.com>
2015-11-24 19:59:29 +0100

    First commit

---
+++ src/main/java/com/ontheroad/jaxrs/provider/MyJacksonProvider.java
@@ -0,0 +1,36 @@
+package com.ontheroad.jaxrs.provider;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Created by Kouinou on 24/11/2015.
+ */
+@Provider
+//@Produces({MediaType.APPLICATION_JSON})
+//@Consumes(MediaType.APPLICATION_JSON)
+//@Singleton
+public class MyJacksonProvider implements ContextResolver<ObjectMapper> {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        MAPPER.disable(MapperFeature.USE_GETTERS_AS_SETTERS);
+        //MAPPER.disable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+    }
+
+    public MyJacksonProvider() {
+        System.out.println("Instantiate MyJacksonProvider");
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        System.out.println("MyJacksonProvider.getContext() called with type: "+type);
+        return MAPPER;
+    }
+}
